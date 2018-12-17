5827bfb9a7d5f708350b0b45c3e9502b617711bc
mohammad-adk <44952214+mohammad-adk@users.noreply.github.com>
2018-12-18 01:40:32 +0330

    Add files via upload

---
+++ app/src/main/java/com/dsproject/gondum/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.gameReset();
+        game.board[1][2][1] = 1;
+        game.board[1][2][2] = 1;
+        game.board[1][2][0] = 1;
+        System.out.println(game.evaluate(1, 2, 1));
+        game.boardPrint();
+
+
+    }
+}
