0bb5c9b6f7b389eab1f802a018a0b4bc9391eac5
Ali Najafi <39293965+AliNajafi1998@users.noreply.github.com>
2018-12-29 09:48:57 +0330

    Nothing special
    Android issues

--- app/src/main/java/com/dsproject/gondum/StartActivity.java
+++ app/src/main/java/com/dsproject/gondum/StartActivity.java
@@ -1,9 +1,11 @@
 package com.dsproject.gondum;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,6 +17,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        changeStatusBarColor();
         img = findViewById(R.id.img);
         img.animate().alpha(1f).setDuration(5000);
         Toast.makeText(this, "Click anywhere to continue...", Toast.LENGTH_LONG).show();
@@ -25,5 +28,12 @@ public class StartActivity extends AppCompatActivity {
         startActivity(new Intent(this, MainMenuActivity.class));
         finish();
 
+    }
+
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
     }
 }
